75b06c4525e9ce6d0ef3bd476b3c7ed1dda51b50
Christian Gröling <contact@christiangroeling.de>
2023-07-30 21:25:44 +0200

    Better examples

---
+++ 04_movecopy/src/lib.rs
@@ -0,0 +1,60 @@
+#[derive(Debug)]
+struct TestStruct {}
+
+#[allow(dead_code)]
+impl TestStruct {
+    fn test_mutable_reference(&self) {
+        // This function wants a mutable borrow of self.
+    }
+
+    fn test_move_ownership(self) {
+        // This function moves self and makes the copy mutable.
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn try_mulitple_immutable_borrows() {
+        // multiple immutable borrows are allowed
+        let a = 123;
+        let a_1 = &a;
+        let a_2 = &a;
+        let a_3 = &a;
+        // let a_4 = &mut a; // E0502 - not allowed
+
+        assert_eq!(a, 123);
+        assert_eq!(*a_1, 123); // no auto deref on i32 therefore asterik (*) is needed
+        assert_eq!(*a_2, 123);
+        assert_eq!(*a_3, 123);
+    }
+
+    #[test]
+    fn try_one_mutable_borrow() {
+        // one mutable borrows are allowed
+        let mut a = 123;
+        let a_1 = &mut a;
+        // let a_2 = &mut a; // E0499 - not allowed
+        // let a_3 = &a; // E0502 - not allowed
+
+        *a_1 = 321;
+
+        assert_eq!(a, 321);
+    }
+
+    #[test]
+    fn try_mutable_reference_and_move_ownership() {
+        // difference between &self and self
+        let lifetime1 = TestStruct {};
+
+        lifetime1.test_mutable_reference();
+        lifetime1.test_move_ownership();
+        // This is not possible anymore:
+        // lifetime1.test_move_ownership(); // E0382
+        // The ownership was moved to test_move_ownership and here it has gone out of scope
+
+
+    }
+}

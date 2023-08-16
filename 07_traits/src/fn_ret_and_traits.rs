be51c09558113e8c809d68147b5fb2407cbd81e4
cgg <christian.groeling@festo.com>
2023-08-16 13:39:18 +0200

    Add new fn ret traits

--- 07_traits/src/fn_ret_and_traits.rs
+++ 07_traits/src/fn_ret_and_traits.rs
@@ -27,6 +27,7 @@ impl Debug for dyn MyErrorTrait {
     }
 }
 
+#[derive(Debug)]
 struct MyError1 {
     msg: String,
 }
@@ -38,7 +39,7 @@ impl MyErrorTrait for MyError1 {
     }
 }
 
-
+#[derive(Debug)]
 struct MyError2 {
     msg: String,
 }
@@ -58,6 +59,25 @@ fn returning_result_dyn(switch: bool) -> Result<i32, Box<dyn MyErrorTrait>> {
     }
 }
 
+// This "fn returning_result_static_2<T:MyErrorTrait>(switch: bool) -> Result<i32,T> " is not possible. 
+// Reason: A user could call returning_result_static_2::<blabla>. But just the same class Err(MyError(...)) is returned. The 
+// function should be univsal but its not.
+//
+// In argument position, impl Trait is very similar in semantics to a generic type parameter. However, there are 
+// significant differences between the two in return position. With impl Trait, unlike with a generic type parameter, 
+// the function chooses the return type, and the caller cannot choose the return type.
+fn returning_result_static_2(switch: bool) -> Result<i32, impl MyErrorTrait+Debug>{ // MyErrorTrait and Debug should be realized 
+
+    // interstingly the debug trait of MyError1 is called by returning_result_static 2
+
+    if (switch) {
+        return Err(MyError1{msg:"MyError1 - hello world".to_string()});
+    } else {
+        return Err(MyError1{msg:"MyError1 - hello world".to_string()}); // Return MyError2 here is not possible ... Statically only one type can be returned
+    }
+
+}
+
 //--------------
 pub fn fn_ret_and_traits() {
     banner::print_h0("Returning traits - statically and dynamically");
@@ -65,4 +85,5 @@ pub fn fn_ret_and_traits() {
     println!("returning_result_static:{:?}", returning_result_static());
     println!("returning_result_dyn:{:?}", returning_result_dyn(true));
     println!("returning_result_dyn:{:?}", returning_result_dyn(false));
+    println!("returning_result_static_2:{:?}", returning_result_static_2(false));
 }

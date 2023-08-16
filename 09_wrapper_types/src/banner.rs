8f0140582a249a2244f7b030227c795272571dab
cgg <christian.groeling@festo.com>
2023-08-16 13:55:54 +0200

    Refactoring

--- 09_wrapper_types/src/banner.rs
+++ 09_wrapper_types/src/banner.rs
@@ -1,9 +1,11 @@
+#[allow(dead_code)]
 pub fn print_h0(title:&str) {
     println!();
     println!("{}",title);
     println!("============================================================");
 }
 
+#[allow(dead_code)]
 pub fn print_h1(title:&str) {
     println!();
     println!("{}",title);

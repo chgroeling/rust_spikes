05238f77fc4503291b9be08d51797ea761139b2c
cgg <christian.groeling@festo.com>
2023-08-17 08:46:53 +0200

    Added static lifetime

---
+++ 04_lifetimes/src/banner.rs
@@ -0,0 +1,13 @@
+#[allow(dead_code)]
+pub fn print_h0(title:&str) {
+    println!();
+    println!("{}",title);
+    println!("============================================================");
+}
+
+#[allow(dead_code)]
+pub fn print_h1(title:&str) {
+    println!();
+    println!("{}",title);
+    println!("------------------------------------------------------------");
+}

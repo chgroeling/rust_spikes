e36edf42e796ffe127f32b01b8bf6b8ee087781e
cgg <christian.groeling@festo.com>
2023-08-17 08:48:36 +0200

    Beautified code

---
+++ 04_lifetimes/src/static_lifetime.rs
@@ -0,0 +1,32 @@
+use crate::banner;
+
+static NUM:i32 = 30; // global
+
+#[allow(dead_code)]
+const CONST:i32= 31; // constant
+
+
+fn work_with_static<'a>(_arg:&'a i32) -> &'a i32 {
+    // Returns a reference to `a_static` where its `'static`
+    // lifetime is coerced to that of the input argument.
+    return &NUM;
+}
+
+pub fn static_lifetime() {
+
+    {
+        banner::print_h0("Static lifetime");
+
+
+        let i_static_1: &'static i32 = &NUM;
+        let str_static: &'static str = "Hello world";
+        let i_static_2: &'static i32 = &5; // reference to a constant number in program memory
+
+        {
+            let _i : i32 = 50;
+            let _b = work_with_static(&NUM);
+        }
+        // a_static stays accessible
+        println!("{i_static_1} {str_static} {i_static_2}");
+    }
+}
